6e8440842e94e215919c3f416233da5f834a1811
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2017-12-04 20:41:01 +0700

    upload

--- application/modules/Article/views/V_mariberiso.php
+++ application/modules/Article/views/V_mariberiso.php
@@ -7,12 +7,15 @@
           
             <div class="regular-content">
               <header>
-                  <h3><span class="text--semi-bold">MARI BER ISO</span></h3>
+                  <h3>
+                      <span class="text--semi-bold">MARI BER ISO</span>
+                      <br>
+                      <span class="text--light">Apa itu ISO ?</span>
+                  </h3>
               </header>
-              <p>Dalam ISO 9001 : 2015 yang mengedepankan Manajemen Berbasis Risiko (Risk Based-Thinking), 
-                  <span style="color: #4286f4"><b>Konteks Organisasi</b></span> adalah salah satu klausul yang termasuk di dalam ISO 9001 : 2015, 
-                 sebelum kita melanjutkan pembahasan ini kita terlebih dahulu memahami arti kata konteks 
-                 organisasi sebagai berikut :
+                <p><span style="color: #4286f4"><b>ISO</b></span> ( International Organization for Standardization ), <span style="color: #4286f4"><b>ISO</b></span> bukan merupakan singkatan. 
+                    <span style="color: #4286f4"><b>ISO</b></span> berasal dari bahasa Yunani yang artinya <span style="color: #4286f4"><b>“SAMA”</b></span>. Hanya saja istilah ini sudah umum 
+                 di dunia organisasi maupun industri digunakan sebagai istilah standarisasi.
               </p>
               
             </div>
