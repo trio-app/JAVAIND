cbdca9932fd2fac1cda1763a84ecbbda785b7d07
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2017-12-04 20:31:19 +0700

    upload

---
+++ application/modules/Article/views/V_mariberiso.php
@@ -0,0 +1,24 @@
+<?php echo $header; ?>
+<br>
+<br>
+<br>
+<div class="container">
+        <div class="row">
+          
+            <div class="regular-content">
+              <header>
+                  <h3><span class="text--semi-bold">MARI BER ISO</span></h3>
+              </header>
+              <p>Dalam ISO 9001 : 2015 yang mengedepankan Manajemen Berbasis Risiko (Risk Based-Thinking), 
+                  <span style="color: #4286f4"><b>Konteks Organisasi</b></span> adalah salah satu klausul yang termasuk di dalam ISO 9001 : 2015, 
+                 sebelum kita melanjutkan pembahasan ini kita terlebih dahulu memahami arti kata konteks 
+                 organisasi sebagai berikut :
+              </p>
+              
+            </div>
+         
+        </div>
+      </div>
+
+
+<?php echo $footer; ?> 
