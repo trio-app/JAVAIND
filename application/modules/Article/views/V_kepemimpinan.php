45b554d38c7d188d1d5daf58dbd3c9300ecd5f1f
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2017-12-04 21:33:09 +0700

    Article
    -Mari Ber ISO
    -Kepemimpinan

---
+++ application/modules/Article/views/V_kepemimpinan.php
@@ -0,0 +1,84 @@
+<?php echo $header; ?>
+<br>
+<br>
+<br>
+<div class="container">
+        <div class="row">
+          
+            <div class="regular-content">
+              <header>
+                  <h3>
+                      <span class="text--semi-bold">KEPEMIMPINAN ISO 9001:2015</span>
+                  </h3>
+              </header>
+                <p>
+                    Dari sejak dahulu ISO menjadikan kepemimpinan sebagai salah satu prinsip manajemen mutu yg melandasi persyaratan - persyaratan ISO 9001. Memang, suatu organisasi tanpa
+                    adanya <span style="color: #4286f4"><b>pemimpin</b></span> tidak akan berjalan, dan <span style="color: #4286f4"><b>pemimpin</b></span> yang tidak memiliki jiwa <span style="color: #4286f4"><b>“kepemimpinan”</b></span> tidak akan mampu memberikan hasil yang diinginkan oleh organisasi itu sendiri. 
+                    Oleh karena itu pada ISO - 9001:2015, konsep kepemimpinan secara explisit dimunculkan sebagai sebuah persyaratan dalam ISO 9001:2015, yaitu pada klausul 5.1.1 dan
+                    klausul 5.1.2 <span style="color: #4286f4"><b>“Manajemen Puncak Harus Menunjukkan Kepemimpinan”</b></span>.
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b>5.1.1 <i>General</i></b></span>
+                    <br>
+                    5.1.1 Umum
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b><i>Top management shall demonstrate leadership and commitment with respect to the quality
+                    management system by:</i></b></span>
+                    <br>
+                    Manajemen puncak harus menunjukkan kepemimpinan dan komitmen terhadap sistem manajemen mutu.
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b>5.1.1 <i>Customer Focus</i></b></span>
+                    <br>
+                    5.1.1 Focus Pelanggan
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b><i>Top management shall demonstrate leadership and commitment with respect to customer focus
+                    by ensuring that:</i></b></span>
+                    <br>
+                    Manajemen puncak harus menunjukkan kepemimpinan dan komitmen terhadap fokus pelanggan dengan memastikan bahwa:
+                </p>
+                <br>
+                <p>
+                    Persyaratan tersebut seakan-akan ingin menekankan kepada pengguna ISO 9001:2015 bahwa sistem manajemen mutu hanya akan efektif 
+                    jika terdapat komitmen pimpinan puncak.Jadi, selain menjalankan fungsi manajemen, pimpinan puncak juga harus memiliki jiwa 
+                    kepemimpinan untuk menjalankan sistem manajemen mutu ISO 9001:2015 yang diterapkan oleh organisasi perusahaan.
+                </p>
+                <br>
+                <p>
+                    Kepemimpinan sangat diperlukan dalam segala hal termasuk manajemen, pimpinan puncak yang memiliki skill di bidang manajemen dan
+                    memiliki jiwa kepemimpinan, akan sangat penting untuk keefektifan sistem manajemen mutu yang diterapkan dalam mencapai target – 
+                    target yang telah ditetapkan oleh organisasi.
+                </p>
+                <br>
+                <p>
+                    Dalam konteks ISO 9001:2015, nampak jelas standar memandangnya berbeda tentang <span style="color: #4286f4"><b>manajemen & kepemimpinan</b></span>, maka dapat ditemukan 
+                    bahwa umumnya <span style="color: #4286f4"><b>“manajemen”</b></span> dipandang berhubungan erat dengan menetapkan kebijakan, mengendalikan kondisi organisasi, mengatur 
+                    sumber daya, memecahankan masalah, & mengelola anggaran. Pada sisi lain, <span style="color: #4286f4"><b>“kepemimpinan”</b></span> berhubungan dengan arah organisasi, 
+                    bagaimana pimpinan mampu menggerakkan personel yg dipimpinnya untuk mencapai sasaran atau target yang telah ditetapkan. 
+                    Jadi, kepemimpinan berhubungan dengan visi dan menjalankan atau memotivasi. Tentu saja, segala sesuatu yang ditetapkan 
+                    harus dipahami terlebih dahulu oleh pimpinan puncak sebelum disahkan dan disosialisasikan ke semua departemen yang terkait, 
+                    karena seperti yang telah dijelaskan di atas, piminan puncak harus mampu mengarahkan dan menggerakkan semua personel yang terkait di dalam organisasi.
+                </p>
+                <br>
+                <p>
+                    Anda seorang pimpinan puncak ? Organisasi anda ingin menerapkan ISO 9001:2015 ? Anda butuh jasa konsultasi ISO ? Hubungi kami,……………………… siap membantu.
+                </p>
+                <br>
+                <p>
+                    Salam sukses & semangat kepemimpinan.
+                </p>
+                
+              
+            </div>
+         
+        </div>
+</div>
+
+
+<?php echo $footer; ?> 
--- application/modules/Article/views/V_mariberiso.php
+++ application/modules/Article/views/V_mariberiso.php
@@ -16,12 +16,60 @@
                 <p><span style="color: #4286f4"><b>ISO</b></span> ( International Organization for Standardization ), <span style="color: #4286f4"><b>ISO</b></span> bukan merupakan singkatan. 
                     <span style="color: #4286f4"><b>ISO</b></span> berasal dari bahasa Yunani yang artinya <span style="color: #4286f4"><b>“SAMA”</b></span>. Hanya saja istilah ini sudah umum 
                  di dunia organisasi maupun industri digunakan sebagai istilah standarisasi.
-              </p>
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b>ISO</b></span> dibentuk di kota Jenewa, Swiss pada tanggal 23 Februari 1947, <span style="color: #4286f4"><b>ISO</b></span> merupakan suatu organisasi di luar pemerintahan (Non-Government Organization/NGO), tujuan dibentuknya
+                    organisasi ini adalah untuk menyamaratakan standarisasi organisasi, industri dan perusahaan – perusahaan dalam skala internasional. Misi dari <span style="color: #4286f4"><b>ISO</b></span> adalah untuk mendukung
+                    pengembangan standarisasi dan kegiatan-kegiatan terkait lainnya dengan harapan untuk membantu perdagangan internasional, dan juga untuk membantu pengembangan kerjasama
+                    secara global di bidang ilmu pengetahuan, teknologi dan juga kegiatan ekonomi.
+                </p>
+                <br>
+                <p>
+                    Jadi, apabila kita sudah menjalankan <span style="color: #4286f4"><b>ISO</b></span> berarti organisasi kita sudah bertaraf internasional dan bisa bersaing secara global.
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b>ISO apakah yang umum diterapkan ?</b></span>
+                </p>
+                <br>
+                <p>
+                    <span style="color: #4286f4"><b>Badan akreditasi ISO</b></span> mengeluarkan sejumlah  <span style="color: #4286f4"><b>sistem manajemen ISO</b></span> yang telah diterapkan oleh organisasi, industri dan perusahaan. Antara lain sebagai berikut :
+                </p>
+                <br>
+                <p>
+                    1. <span style="color: #4286f4"><b>ISO 9001</b></span> tentang manajemen mutu.
+                    <br>
+                    2. <span style="color: #4286f4"><b>ISO 14001</b></span> tentang manajemen pengelolaan lingkungan.
+                    <br>
+                    3. <span style="color: #4286f4"><b>OHSAS 18001</b></span> tentang kesehatan dan keselamatan Kerja.
+                    <br>
+                    4. <span style="color: #4286f4"><b>ISO 22000</b></span> tentang manajemen keamanan pangan.
+                    <br>
+                    5. <span style="color: #4286f4"><b>ISO/TS 16949</b></span> tentang manajemen penyediaan material untuk industri.
+                    <br>
+                    6. <span style="color: #4286f4"><b>ISO 17025</b></span> tentang kegiatan laboratorium.
+                    <br>
+                    7. <span style="color: #4286f4"><b>ISO 13485</b></span> tentang industri peralatan medis.
+                    <br>
+                    8. <span style="color: #4286f4"><b>ISO 28000</b></span> tentang kemanan rantai pasokan industri.
+                </p>
+                <br>
+                <p>
+                    Itulah penjelasan singkat mengenai <span style="color: #4286f4"><b>ISO</b></span>, nantikan artikel – artikel berikutnya di website kami www……………….web.id
+                    <br>
+                    Mari ber <span style="color: #4286f4"><b>ISO</b></span>, untuk organisasi yang lebih baik dan lebih kompetitif.
+                    <span style="color: #4286f4"><b>Anda atau organisasi membutuhkan konsultasi ISO ???</b></span> kami siap membantu sampai proses sertifikasi.
+                </p>
+                <br>
+                <p>
+                    Salam sukses dan semangat.
+                </p>
               
             </div>
          
         </div>
-      </div>
+</div>
 
 
 <?php echo $footer; ?> 
